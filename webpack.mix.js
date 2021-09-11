be71cfa1c1fa688f69d3c7fe7052fbb64041bc17
geekfil <geekfil@gmail.com>
2021-09-11 23:40:09 +0300

    add browserSync; fix pug config

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,21 +4,25 @@
 
 const mix = require("laravel-mix");
 const path = require("path");
+mix.pug = require("laravel-mix-pug-recursive");
 require("laravel-mix-clean");
 require("laravel-mix-polyfill");
 require("laravel-mix-purgecss");
 require("laravel-mix-tailwind");
 
-mix.pug = require("laravel-mix-pug-recursive");
-const LiveReloadPlugin = require("webpack-livereload-plugin");
-
 mix.setPublicPath("dist");
-mix.js("src/scripts/app.js", "assets/app.js");
-mix.sass("src/styles/app.sass", "assets/app.css");
-mix.pug("src/pug/**/*.pug", "dist", { excludePath: "src/pug" });
+mix.js("src/scripts/app.js", "assets");
+mix.sass("src/styles/app.sass", "assets");
+mix.pug("src/pug/template/*.pug", "dist", { excludePath: "src/pug/template" });
 mix.clean();
 // Создание vendor.js с модулями
 mix.extract(["jquery"]);
+
+mix.browserSync({
+  watch: true,
+  files: "dist",
+  proxy: "localhost:8080",
+});
 
 if (mix.inProduction()) {
   mix.polyfill({
